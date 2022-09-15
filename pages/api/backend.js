7926b8b7abdda260c35cbfa7b6beb1b91aa6d9c1
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-09-16 02:34:39 +0600

    online active

--- pages/api/backend.js
+++ pages/api/backend.js
@@ -1,3 +1,3 @@
-const backend = "http://localhost:3200/";
-// const backend = "https://server42223.herokuapp.com/";
+//const backend = "http://localhost:3200/";
+const backend = "https://server42223.herokuapp.com/";
 export default backend;
